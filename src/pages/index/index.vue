06d37da6c0a3483f2e4e05a61f3f6f41f2f962cb
rochurain <631669036@qq.com>
2018-09-12 12:43:33 +0800

    第一次提交

---
+++ src/pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="header">
+      <!-- 搜索栏 -->
+      <icon type="search" size="16"></icon>
+      <input type="text" placeholder="搜索">
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  
+
+}
+</script>
+
+<style lang="less">
+ .header{
+    padding: 20rpx 16rpx;
+    position: relative;
+    background-color: #FF2D4A;
+    icon{
+      position: absolute;
+      top:34rpx;
+      left:50%;
+      transform: translateX(-200%);
+      z-index: 998;
+    }
+    input{
+      height: 60rpx;
+      border-radius: 10rpx;
+      background-color: white;
+      width:100%;
+      color:#bbb;
+      text-align: center;
+      font-size:24rpx;
+    }
+  }
+</style>
